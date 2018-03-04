4ac68660399c66cdeb584f4c8bc5f0960bc9590d
Andrew Lyon <anlyon99@gmail.com>
2018-03-03 23:43:59 -0700

    Added update to MakeChangeApp

--- src/com/skilldistillery/projects/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/projects/makechange/MakeChangeApp.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.projects.makechange;
 
+//simple cash register app that takes a user input for price & how much they payed
+//will printout the correct amount of bills and change to give the customer to use
+//the least amount possible
+
 import java.util.Scanner;
 
 public class MakeChangeApp {
@@ -8,31 +12,54 @@ public class MakeChangeApp {
 		Scanner kb = new Scanner(System.in);
 
 		while (true) {
+			//prompt user for price
+			System.out.println("(To quit the program, simply type 0 for both of the following options.)");
 			System.out.print("Please enter the price of the item: ");
 			double itemPrice = kb.nextDouble();
 
+			//prompt user for payment amount
 			System.out.print("Please give me money: ");
 			double amountPayed = kb.nextDouble();
-
+			
+			
+			//set if statement to end app
+			if (amountPayed == 0 && itemPrice == 0) {
+				System.out.println("Thank you, have a nice day!");
+				break;
+			}
+			//set if statement that informs the customer if they didn't pay enough
 			if (amountPayed < itemPrice) {
 				System.out.println("Sorry, that is not enough money.");
 				continue;
 			}
+			//set if statement for exact change
+			if (amountPayed == itemPrice) {
+				System.out.println("Oh wow, that's exact change! Thank you, have a nice day!");
+				break;
+			}
 
+			//send doubles to decideChange method
 			decideChange(itemPrice, amountPayed);
 		}
+		
+		kb.close();
 	}
 
+	//this method decides how much change to give the customer as well as print out the results
 	public static void decideChange(double price, double payed) {
 		int twentyDollars = 2000, tenDollars = 1000, fiveDollars = 500, oneDollar = 100, quarter = 25, dime = 10,
 				nickel = 5, penny = 1;
 
+		//multiply entered numbers by 100 and set to int for easier calculations
 		int itemPrice = (int) (price * 100);
 		int amountPayed = (int) (payed * 100);
 
-		System.out.println("price " + itemPrice + " payed " + amountPayed);
-
+		//calculate the difference between the amount payed and the price
+		//set that number to a new variable
 		int change = (int) (amountPayed - itemPrice);
+		
+		//calculate the amount of change needed for each type of currency
+		//set change to new amount of leftover via % 
 		int twentyChange = change / twentyDollars;
 		change = change % twentyDollars;
 		int tenChange = change / tenDollars;
@@ -49,15 +76,18 @@ public class MakeChangeApp {
 		change = change % nickel;
 		int pennyChange = change / penny;
 		change = change % penny;
-
-		System.out.println("You get " + twentyChange + " $20");
-		System.out.println("You get " + tenChange + " $10");
-		System.out.println("You get " + fiveChange + " $5");
-		System.out.println("You get " + oneChange + " $1");
-		System.out.println("You get " + quarterChange + " $0.25");
-		System.out.println("You get " + dimeChange + " $0.10");
-		System.out.println("You get " + nickelChange + " $0.05");
-		System.out.println("You get " + pennyChange + " $0.01");
+		
+		//print out the change the customer receives & final message
+		System.out.print("You get " + twentyChange + " $20 bills, ");
+		System.out.print(tenChange + " $10 bills, ");
+		System.out.print(fiveChange + " $5 bills, ");
+		System.out.print(oneChange + " $1 bills, ");
+		System.out.print(quarterChange + " quarters, ");
+		System.out.print(dimeChange + " dimes, ");
+		System.out.print(nickelChange + " nickels, ");
+		System.out.println("and " + pennyChange + " pennies.");
+		System.out.println("Have a great day!");
+		System.out.println("");
 	}
 
 }
